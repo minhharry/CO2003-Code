10283f4238715211da825937a99cab3aff7518ba
minhharry <tbminh@gmail.com>
2023-11-10 14:03:00 +0700

    .

--- AVLtree.cpp
+++ AVLtree.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include<memory>
+#include<string>
 #define MOD 1000000007
 
 #ifdef cbaaabc
@@ -23,21 +23,22 @@ void operator delete(void* memory, size_t size)
     totalFreed += size;
     free(memory);
 }
-class Node
-{
-public:
-    int value = 0;
-    int height = 0;
-    Node* left = nullptr;
-    Node* right = nullptr;
-    Node(){};
-    Node(int value): value(value) {};
-    Node(int value, Node* left, Node* right): value(value), left(left), right(right){};
-
-};
+template<class T>
 class AVLTree
 {
 private:
+    class Node
+    {
+    public:
+        T key = 0;
+        int height = 0;
+        Node* left = nullptr;
+        Node* right = nullptr;
+        Node() {};
+        Node(const T& key): key(key) {};
+        Node(const T& key, Node* left, Node* right): key(key), left(left), right(right) {};
+
+    };
     Node* root;
     static int height(Node* x)
     {
@@ -54,9 +55,9 @@ private:
         Node* temp2 = root->left->right;
         temp->right = root;
         root->left = temp2;
-        root->height = max(height(root->left), height(root->right))+1;
+        root->height = max(height(root->left), height(root->right)) + 1;
         root = temp;
-        root->height = max(height(root->left), height(root->right))+1;
+        root->height = max(height(root->left), height(root->right)) + 1;
     }
     static void rotateLeft(Node* &root)
     {
@@ -64,71 +65,73 @@ private:
         Node* temp2 = root->right->left;
         temp->left = root;
         root->right = temp2;
-        root->height = max(height(root->left), height(root->right))+1;
+        root->height = max(height(root->left), height(root->right)) + 1;
         root = temp;
-        root->height = max(height(root->left), height(root->right))+1;
+        root->height = max(height(root->left), height(root->right)) + 1;
     }
     static void balanceTree(Node* &root)
     {
         int balanceFactor = getBalance(root);
-        if (balanceFactor > 1 && getBalance(root->left)>=0)
+        if (balanceFactor > 1 && getBalance(root->left) >= 0)
         {
             rotateRight(root);
             return;
         }
-        if (balanceFactor > 1 && getBalance(root->left)<=-1)
+        if (balanceFactor > 1 && getBalance(root->left) <= -1)
         {
             rotateLeft(root->left);
             rotateRight(root);
             return;
         }
-        if (balanceFactor < -1 && getBalance(root->right)<=0)
+        if (balanceFactor < -1 && getBalance(root->right) <= 0)
         {
             rotateLeft(root);
             return;
         }
-        if (balanceFactor < -1 && getBalance(root->right)>=1)
+        if (balanceFactor < -1 && getBalance(root->right) >= 1)
         {
             rotateRight(root->right);
             rotateLeft(root);
             return;
         }
     }
-    static Node* insertNode(Node* root, int value)
+    static Node* insertNode(Node* root, const T& key)
     {
-        if (root==nullptr)
+        if (root == nullptr)
         {
-            root = new Node(value);
+            root = new Node(key);
             root->height = 1;
             return root;
         }
-        if (value < root->value)
+        if (key < root->key)
         {
-            root->left = insertNode(root->left, value);
-        } else if (value > root->value)
+            root->left = insertNode(root->left, key);
+        }
+        else if (key > root->key)
         {
-            root->right = insertNode(root->right, value);
-        } else
+            root->right = insertNode(root->right, key);
+        }
+        else
             return root;
 
-        root->height = max(height(root->left), height(root->right))+1;
+        root->height = max(height(root->left), height(root->right)) + 1;
         balanceTree(root);
         return root;
     }
-    static Node* minValueNode(Node* x)
+    static Node* minKeyNode(Node* x)
     {
         Node* current = x;
         while (current->left)
             current = current->left;
         return current;
     }
-    static Node* deleteNode(Node* root, int value)
+    static Node* deleteNode(Node* root, const T& key)
     {
         if (root == nullptr)
         {
             return nullptr;
         }
-        if (value == root->value)
+        if (key == root->key)
         {
             if (root->left == nullptr && root->right == nullptr)
             {
@@ -150,51 +153,61 @@ private:
             }
             else if (root->left != nullptr && root->right != nullptr)
             {
-                Node* temp = minValueNode(root->right);
-                root->value = temp->value;
-                root->right = deleteNode(root->right, temp->value);
+                Node* temp = minKeyNode(root->right);
+                root->key = temp->key;
+                root->right = deleteNode(root->right, temp->key);
             }
-        } else if (value < root->value)
+        }
+        else if (key < root->key)
         {
-            root->left = deleteNode(root->left, value);
-        } else if (value > root->value)
+            root->left = deleteNode(root->left, key);
+        }
+        else if (key > root->key)
         {
-            root->right = deleteNode(root->right, value);
+            root->right = deleteNode(root->right, key);
         }
         if (root)
         {
-            root->height = max(height(root->left), height(root->right))+1;
+            root->height = max(height(root->left), height(root->right)) + 1;
             balanceTree(root);
         }
         return root;
     }
-    static void printTreePreorder(Node* x, int space=0)
+    static void printTreePreorder(Node* x, int space = 0)
     {
-        for (int i = 0; i<space; ++i) cout << " ";
-        if (x) cout << x->value << endl; else cout << "N" << endl;
-        if (x==nullptr) return;
-        printTreePreorder(x->left, space+4);
-        printTreePreorder(x->right, space+4);
+        for (int i = 0; i < space; ++i) cout << " ";
+        if (x) cout << x->key << endl;
+        else cout << "N" << endl;
+        if (x == nullptr) return;
+        printTreePreorder(x->left, space + 4);
+        printTreePreorder(x->right, space + 4);
     }
-    static void printTreeInorder(Node* x, int space=0)
+    static void printTreeInorder(Node* x, int space = 0)
     {
-        if (x==nullptr)
+        if (x == nullptr)
         {
-            for (int i = 0; i<space; ++i) cout << " ";
+            for (int i = 0; i < space; ++i) cout << " ";
             cout << "N" << endl;
             return;
         }
-        printTreeInorder(x->left, space+4);
-        for (int i = 0; i<space; ++i) cout << " ";
-        cout << x->value << endl;
-        printTreeInorder(x->right, space+4);
+        printTreeInorder(x->left, space + 4);
+        for (int i = 0; i < space; ++i) cout << " ";
+        cout << x->key << endl;
+        printTreeInorder(x->right, space + 4);
     }
-    static bool isExist(Node* x, int value)
+    static T findKey(Node* x, const T& key)
     {
-        if (x==nullptr) return false;
-        if (x->value==value) return true;
-        if (x->value > value) return isExist(x->left, value);
-        return isExist(x->right, value);
+        if (x == nullptr) return T();
+        if (x->key == key) return x->key;
+        if (x->key > key) return findKey(x->left, key);
+        return findKey(x->right, key);
+    }
+    static bool isExist(Node* x, const T& key)
+    {
+        if (x == nullptr) return false;
+        if (x->key == key) return true;
+        if (x->key > key) return isExist(x->left, key);
+        return isExist(x->right, key);
     }
 public:
     AVLTree()
@@ -203,34 +216,59 @@ public:
     }
     ~AVLTree()
     {
-        while (root) deleteNode(root->value);
+        while (root) deleteNode(root->key);
     }
-    void insertNode(int value)
+    void insertNode(const T& key)
     {
-        root = insertNode(root, value);
+        root = insertNode(root, key);
     }
-    void deleteNode(int value)
+    void deleteNode(const T& key)
     {
-        root = deleteNode(root, value);
+        root = deleteNode(root, key);
     }
-    void printTree(string type="LNR")
+    void printTree(string type = "LNR")
     {
-        if (type=="LNR") printTreeInorder(root);
-        if (type=="NLR") printTreePreorder(root);
+        if (type == "LNR") printTreeInorder(root);
+        if (type == "NLR") printTreePreorder(root);
     }
-    bool isExist(int value)
+    T findKey(const T& key) const
     {
-        isExist(root, value);
+        return findKey(root, key);
+    }
+    bool isExist(const T& key) const
+    {
+        return isExist(root, key);
     }
 };
+struct Point2D
+{
+    int x, y;
 
+};
+bool operator<(const Point2D &a, const Point2D &b)
+{
+    return a.x < b.x;
+}
+bool operator>(const Point2D &a, const Point2D &b)
+{
+    return a.x > b.x;
+}
+bool operator==(const Point2D &a, const Point2D &b)
+{
+    return a.x == b.x;
+}
+ostream& operator<<(ostream &os, const Point2D &a)
+{
+    os << "(" << a.x << "," << a.y << ")";
+}
 int main()
 {
-    // freopen("debug.out", "w", stdout);
+    //freopen("debug.out", "w", stdout);
     {
-        AVLTree a;
-        for (int i = 0; i<20; ++i) a.insertNode(i);
+        AVLTree<int> a;
+        for (int i = 20; i >= 0; --i) a.insertNode(rand()%100);
         a.printTree();
+        cout << a.isExist(68) << endl;
     }
 
     deb(totalAllocated);
